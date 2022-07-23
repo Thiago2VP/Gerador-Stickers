4782a112ce23911c01b5f8408180b1d544882565
Thiago2VP <thiago_vasconcelosvp@outlook.com>
2022-07-22 22:45:31 -0300

    Aulas Finalizadas

---
+++ src/TratamentoImagem.java
@@ -0,0 +1,23 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+public class TratamentoImagem {
+    public static BufferedImage toBufferedImage(Image img) {
+        if (img instanceof BufferedImage)
+        {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bimage.createGraphics();
+        bGr.drawImage(img, 0, 0, null);
+        bGr.dispose();
+
+        // Return the buffered image
+        return bimage;
+    }
+}
